00dff36fe3bdc812ff10da23076c0e035293a251
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-06-30 17:19:38 +0700

    debug assets netlify

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,11 +6,7 @@
       <div
         class="flex flex-col md:flex-row justify-center items-center py-14 gap-8"
       >
-        <img
-          class="w-56 h-56 rounded-full"
-          src="../assets/headshot.jpg"
-          alt=""
-        />
+        <img class="w-56 h-56 rounded-full" src="/headshot.jpg" alt="" />
         <div class="text-left space-y-4">
           <h1 class="text-3xl md:text-5xl font-semibold">I'm Iqbal, Hello !</h1>
           <p>Frontend Developer from South East Asia</p>
